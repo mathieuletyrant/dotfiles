d81f5c6908d90c23fd942b2ca307f98cbd230d66
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2024-10-22 09:22:27 +0200

    IEize

--- macos.sh
+++ macos.sh
@@ -10,7 +10,10 @@ osascript -e 'tell application "System Preferences" to quit'
 # Save screenshots in JPG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
-# Finder: show status bar
+#  Finder show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
 # Disable the warning when changing a file extension
@@ -23,12 +26,16 @@ defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 # Disable 'natural' scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
+killall Finder &> /dev/null
+
 ###############################################################################
 # Dock
 ###############################################################################
 
 # Disable recent apps in Dock
 defaults write com.apple.dock show-recents -bool false
+
+killall Dock &> /dev/null
 
 ###############################################################################
 # Keyboard
