2285efce0f1ef09df805a31675577b2cd7f9bef7
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2024-11-01 16:08:25 +0100

    style: cosmetic

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Oh My Zsh installed!"
+echo "=> Oh My Zsh installed!"
 
 # Install applications
 brew bundle install
