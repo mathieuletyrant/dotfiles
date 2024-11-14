df0c4b4ca72486aef00e3e09b86584c9fdeb1077
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2024-11-14 07:51:16 +0100

    chore(brewfile): replace wezterm with warp terminal

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "=> Applications installed!"
 
 # Symlink using Stow
 stow git --adopt
-stow wezterm --adopt
+# stow wezterm --adopt
 stow zsh --adopt
 stow oh-my-posh --adopt
 stow config --adopt
